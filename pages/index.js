3e20e52f31551849bbebc3a7a2f67c35ecaeda9e
bratishques <gurda999@gmail.com>
2020-12-22 21:46:52 +0300

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,146 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
+  const [audioLink, setAudioLink] = useState(
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/shoptalk-clip.mp3"
+  );
+  const [audioFile, setAudioFile] = useState(null);
+  const [audioContextState, setAudioContextState] = useState(null);
+  const [audioLoading, setAudioLoading] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0)
+  const [borderHeight, setBorderHeight] = useState(150)
+  const [borderWidth, setBorderWidth] = useState(500)
+  const [padding, setPadding] = useState(40)
+
+  useEffect(() => {
+    if (audioContextState) {
+      // filter the data
+      const size = 80;
+      const rawData = audioContextState.getChannelData(0);
+
+      const samples = Math.floor(rawData.length / size);
+      const filteredData = [];
+      for (let i = 0; i < size; i++) {
+        let start = i * samples;
+        let sum = 0;
+        for (let j = 0; j < samples; j++) {
+          sum = sum + Math.abs(rawData[start + j]);
+        }
+        filteredData.push(sum / samples);
+      }
+      console.log(filteredData);
+
+      // equailize the data
+      let multiplier = 1 / Math.max(...filteredData);
+      const equalizedData = filteredData.map((a) => a * multiplier);
+      console.log(equalizedData);
+
+      // get visuals done
+      const canvas = document.getElementById("line-canvas");
+      canvas.width = borderWidth + padding;
+      canvas.height = borderHeight + padding;
+      const ctx = canvas.getContext("2d");
+      ctx.translate(0, borderHeight+padding-10);
+
+      //draw the bottom line
+      const drawBottomLine = () => {
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(canvas.width, 0);
+        ctx.stroke();
+      };
+      drawBottomLine();
+
+      //prepare the lines
+      const stroke = borderWidth / size - 2;
+
+      const drawRect = (sample, stroke, xStart) => {
+        ctx.lineWidth = stroke;
+        ctx.beginPath();
+        ctx.moveTo(xStart, 0);
+        ctx.lineTo(xStart, -(borderHeight * sample));
+        ctx.stroke();
+      };
+
+      for (let i = 0; i < equalizedData.length; i++) {
+        const xStart = (borderWidth / size) * i + padding / 2;
+        drawRect(equalizedData[i], stroke, xStart);
+      }
+    }
+  }, [audioContextState]);
+
+  const audioCallback = useCallback((e) => {
+
+  })
+
+  useEffect(() => {
+    if (audioFile) {
+    const audio = document.querySelector("audio")
+    setInterval(() => {
+      setCurrentTime(audio.currentTime)
+      console.log(audio.currentTime)
+    }, 100)
+    const overlay = document
+  }
+  }, [audioFile]);
+
+
+  const fetchAudio = async () => {
+    setAudioContextState(null);
+    if (window) {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      const audioContext = new AudioContext();
+      setAudioLoading(true);
+      const response = await fetch(audioLink);
+      const buffer = await response.arrayBuffer();
+      const decoded = await audioContext.decodeAudioData(buffer);
+      console.log(decoded);
+      setAudioContextState(decoded);
+      setAudioFile(response);
+      setAudioLoading(false);
+    }
+  };
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Audio Visualiser</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <h1>Audio Visualiser</h1>
         </div>
+        <div>
+          <label htmlFor="link">
+            Please enter an audio link:
+            <input
+              type="link"
+              id="link"
+              value={audioLink}
+              onChange={(e) => {
+                setAudioLink(e.target.value);
+              }}
+            ></input>
+          </label>
+          <button onClick={fetchAudio}>Submit</button>
+        </div>
+        {audioLoading && <div>...loading</div>}
+        <canvas id="line-canvas"></canvas>
+        <canvas id="overlay-canvas"></canvas>
+        {audioFile && (
+          <div>
+            <audio src={audioLink} controls></audio>
+            <div>
+
+            </div>
+          </div>
+        )}
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <footer></footer>
     </div>
-  )
+  );
 }
